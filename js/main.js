26074745886c4ff772ffbf0886386b02572c9d3b
EvgeniyKvasov <eakvasov@yandex.ru>
2026-03-11 18:48:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    
+    $('.add-to-cart').click(function() {
+        let product = $(this).data('product');
+        
+        $('#cartMessage')
+            .text('✅ ' + product + ' добавлен в корзину')
+            .show(); 
+    });
+    
+});
